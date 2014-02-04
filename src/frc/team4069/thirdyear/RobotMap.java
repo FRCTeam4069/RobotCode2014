822e67023c6f0e3d2ff915992391a3b0ec43eefb
silverballer <edmundnoble@gmail.com>
2014-02-03 21:40:03 -0500

    First commit
    Added initial robot code: makeshift subsystems and commands, no real
    shooter.

---
+++ src/frc/team4069/thirdyear/RobotMap.java
@@ -0,0 +1,21 @@
+package frc.team4069.thirdyear;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public interface RobotMap {
+    
+    /** 
+     *  Left motor port.
+     */
+     public static final int LEFT_MOTOR = 1;
+     
+     /**
+     *  Right motor port.
+     */
+    public static final int RIGHT_MOTOR = 2;
+    
+}

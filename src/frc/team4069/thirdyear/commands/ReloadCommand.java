6dc7185a9de52c4e12506bc80179169c85608fe7
silverballer <edmundnoble@gmail.com>
2014-02-03 21:44:41 -0500

    First real commit
    Added robot code; skeletal subsystems. No shooter. When a blob is
    detected, the robot moves forward. No teleop.

---
+++ src/frc/team4069/thirdyear/commands/ReloadCommand.java
@@ -0,0 +1,37 @@
+package frc.team4069.thirdyear.commands;
+
+/**
+ * This will be implemented with the Shooter subsystem.
+ *
+ * @author Edmund
+ */
+public class ReloadCommand extends CommandBase {
+
+    public ReloadCommand() {
+        requires(shooter);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
